2a1069c6d5ade1d454ac8f1ff9c2302bf9d1dadf
nilsh <nils.henningsen@freenet.de>
2024-08-11 21:15:53 +0200

    removed not needed stuff

--- ServerCAME/ServerCAME/Main.cpp
+++ ServerCAME/ServerCAME/Main.cpp
@@ -1,8 +1,7 @@
 #include "Server.h"
 int main(int argc, char* argv[])
 {
-	std::vector<std::vector<std::string>> baseArr(10, std::vector<std::string>(2));
-	Server server(baseArr);
+	Server server;
 	server.InitServer(argc, argv);
 	return 0;
 }
