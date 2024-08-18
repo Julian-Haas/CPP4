dddb9eef91dfc8cab1434a86101e1d5777d3c994
Julian-Haas <julian.haas@web.de>
2024-08-18 16:45:41 +0200

    setupnetwork class

---
+++ MicroEngine/inc/MicroEngine/setupnetwork.h
@@ -0,0 +1,10 @@
+#ifndef SETUPNETWORK_H
+#define SETUPNETWORK_H
+
+class SetupNetwork {
+public:
+	bool Connect();
+	void CreateServer();
+};
+
+#endif // SETUPNETWORK_H
---
+++ MicroEngine/src/setupnetwork.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include "setupnetwork.h"
+
+bool SetupNetwork::Connect() {
+	Beep(750, 300);
+	// Attempt to connect
+	// Return if connection was successful
+	return false;
+}
+
+void SetupNetwork::CreateServer() {
+	// Create server logic here
+}
